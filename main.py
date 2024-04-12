654f36d84c78db7693034e65717dfc3c1f4b216b
ons-ou <onsouahchi@gmail.com>
2024-04-12 18:48:38 +0100

    hello

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy for sure")
+    return jsonify("Healthy for sure!!")
 
 
 @APP.route('/auth', methods=['POST'])
